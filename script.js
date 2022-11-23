699ef7c57378dcac3ffc7f025f0fbb73cb1b4c5f
tblouir <tblouirschool@gmail.com>
2022-11-23 16:35:31 -0700

    Add sun animation, fade-in, mobile CSS

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const mainContainer = document.querySelector('.main-container')
+const body = document.querySelector('body')
+const sunstick = document.getElementById('sun-stick')
+const sun = document.getElementById('sun')
+
+sunstick.addEventListener("animationend", e => {
+  console.log('finishing');
+  sun.remove()
+  sunstick.remove()
+  body.style.overflowY = 'auto'
+})
